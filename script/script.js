f073d541130a68a3f4faa843342d3ef0e99d50fa
Gaurav Tamang <gaurav.tmg21@gmail.com>
2021-09-03 21:26:33 +0545

    first commit

---
+++ script/script.js
@@ -0,0 +1,4 @@
+gsap.from('article',{duration: 1, y: '100vh', opacity: 0, ease: 'circ'});
+// gsap.from('.card',{duration:1, y: '100vh', opacity: 0, stagger: 0.5, ease: 'power1.inOut'})
+gsap.from('.image',{duration: 1, x: '-100vw', ease: 'power1.out', opacity: 0})
+gsap.from('.intro',{duration: 1.5, opacity: 0, ease: 'power1.in', delay: 1})
